84ef67581704265b505c4c9741c4a1d8eba0f27f
Freusturz <askerbelkin@mail.ru>
2023-02-21 21:48:15 +0300

    Выполненная домашняя работа по "OOP2" Ilya Shchipletsov

---
+++ src/Color.java
@@ -0,0 +1,18 @@
+public enum Color {
+    White("Белый"),
+    Yellow("Желтый"),
+    Black("Черный"),
+    Brown("Коричневый"),
+    Pink("Розовый");
+
+    private final String colorName;
+
+    Color(String colorName) {
+        this.colorName = colorName;
+    }
+
+    @Override
+    public String toString() {
+        return colorName;
+    }
+}
